46d05ec6c9e3fc7a3ce1357823ebd7d30cee23f7
Максим Белоглазов <maksimbeloglazov@MacBook-Pro.local>
2018-10-24 12:59:20 +0300

    deleted mult1259

--- CalcStarter.java
+++ CalcStarter.java
@@ -6,7 +6,5 @@ public class CalcStarter {
            System.out.println("2 add 1 = " + a);
          int b = MainCalc.sub(2,1);
            System.out.println("2 minus 1 = " + b);
-         int c = MainCalc.mult(2,1);
-           System.out.println("2 mult by 1 = " + c);
     }
 }
